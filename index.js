de737c5623ab647042f7da196ccbcbce5012486f
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2024-01-14 19:37:34 +0000

    updated the typo in function that renders a license badge

--- index.js
+++ index.js
@@ -62,9 +62,10 @@ const questions = [
     },
 ];
 
-const promptUser = () =>
-  inquirer.prompt(
-    );
+
+// const promptUser = () =>
+//   inquirer.prompt(
+//     );
   
 
 
@@ -92,15 +93,6 @@ async function writeToFile(fileName, data) {
         console.error(`Error writing file: ${error.message}`);
     }
 }
-
-    // fs.writeFile("books.txt", data, (err) => {
-    //     if (err)
-    //         console.log(err);
-    //     else {
-    //         console.log("File written successfully\n");
-    //         console.log("The written has the following contents:");
-    //         console.log(fs.readFileSync("books.txt", "utf8"));
-    //     }
 
 // function to initialize program
 function init() {
